0f9a2bc49812f9839d69df398e43939871d65ef8
lsanchezrueda904-hue <lsanchezrueda904@gmail.com>
2026-04-08 12:42:19 -0500

    feat:modificar estante y almacen

--- almacenamiento/views.py
+++ almacenamiento/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import EstanteForm, AlmacenForm
 from .models import Estante, Almacen
 
@@ -8,9 +8,25 @@ def vista_almacenes(request):
     form = AlmacenForm()
 
     if request.method == 'POST':
-        form = AlmacenForm(request.POST)
-        if form.is_valid():
-            form.save()
+        accion = request.POST.get('accion')
+
+        if accion == 'crear':
+            form = AlmacenForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('almacenes')
+
+        elif accion == 'editar':
+            pk = request.POST.get('almacen_id')
+            almacen = get_object_or_404(Almacen, pk=pk)
+            form_editar = AlmacenForm(request.POST, instance=almacen)
+            if form_editar.is_valid():
+                form_editar.save()
+                return redirect('almacenes')
+
+        elif accion == 'eliminar':
+            pk = request.POST.get('almacen_id')
+            get_object_or_404(Almacen, pk=pk).delete()
             return redirect('almacenes')
 
     return render(request, 'almacenes.html', {
@@ -21,18 +37,34 @@ def vista_almacenes(request):
 
 
 def vista_estantes(request):
-    if request.method == 'POST':
-        form = EstanteForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('estantes')
-    else:
-        form = EstanteForm()
-
     estantes = Estante.objects.all()
+    form = EstanteForm()
+
+    if request.method == 'POST':
+        accion = request.POST.get('accion')
+
+        if accion == 'crear':
+            form = EstanteForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('estantes')
+
+        elif accion == 'editar':
+            pk = request.POST.get('estante_id')
+            estante = get_object_or_404(Estante, pk=pk)
+            form_editar = EstanteForm(request.POST, instance=estante)
+            if form_editar.is_valid():
+                form_editar.save()
+                return redirect('estantes')
+
+        elif accion == 'eliminar':
+            pk = request.POST.get('estante_id')
+            get_object_or_404(Estante, pk=pk).delete()
+            return redirect('estantes')
 
     return render(request, 'estantes.html', {
         'estantes': estantes,
+        'almacenes': Almacen.objects.all(),
         'form': form,
         'show_modal': bool(form.errors)
     })
