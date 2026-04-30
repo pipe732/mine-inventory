a67bbcc6b996b31e314693309683cc11244f6bc4
Andres <andres.r.b.1502@gmail.com>
2026-04-30 13:30:51 -0500

    Revert "Merge branch 'main' into usuario"
    This reverts commit 24305810eb04e8ca39713c6f9188771c27129a60, reversing
    changes made to 613bc5b70f958e8edfce9e5b3a77a00aa24e3d9e.

--- core/urls.py
+++ core/urls.py
@@ -3,12 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from usuario.views import login_view
-from prestamo.views import prestamo_api  # ← nueva
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', login_view, name='login'),
-    path('home/', include('pagina_principal.urls')),
+    path('home/', include('pagina_principal.urls')),  # Redirige a la página principal o dashboard de administrador
     path('devoluciones/', include('devoluciones.urls')),
     path('usuario/', include('usuario.urls')),
     path('prestamos/', include('prestamo.urls')),
@@ -16,8 +15,4 @@ urlpatterns = [
     path('almacenamiento/', include('almacenamiento.urls')),
     path('mantenimiento/', include('mantenimiento.urls')),
     path('reportes/', include('reportes.urls')),
-    path('api/prestamos/<int:pk>/', prestamo_api, name='api_prestamo'),  # ← nueva
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- inventario/urls.py
+++ inventario/urls.py
@@ -5,5 +5,4 @@ app_name = 'inventario'
 
 urlpatterns = [
     path('', views.inventario, name='inventario'),
-    path('productos/autocomplete/', views.producto_autocomplete, name='producto_autocomplete'),
 ]
